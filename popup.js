16f6824cca852029675be496d7724b8dc9680407
DrDrunkenstein42 <73171175+DrDrunkenstein42@users.noreply.github.com>
2021-02-19 13:53:48 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,55 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var bg = chrome.extension.getBackgroundPage()
+    var bg = chrome.extension.getBackgroundPage();
 
     document.getElementById("Air").textContent = bg.airPollution;
     document.getElementById("Water").textContent = bg.waterPollution;
+    /*
+    console.log("Water Pollution:")
+    console.log(bg.waterPollution);
+    console.log("Air Pollution:")
+    console.log(bg.airPollution);
+    */
 
-    if (!(window.treeFigure === null)) {
-        // have to create div element and set its textContent to the treeFigure
+
+    if (!(window.treeFigure === 0 || window.treeFigure === undefined)) {
+
+        var treeDiv = document.createElement("div");
+        var className = document.createAttribute("class");
+        var id = document.createAttribute("id");
+        id.value = "tree";
+        className.value = "container";
+        treeDiv.setAttributeNode(className);
+        treeDiv.setAttributeNode(id);
+        treeDiv.textContent = "CO2 absorbed annually by trees cut";
+        document.getElementsByClassName("mainContainer")[0].append(treeDiv);
+
+
+        var treeValue = document.createElement("div");
+        var newClass = document.createAttribute("class");
+        newClass.value = "values";
+        var newId = document.createAttribute("id");
+        newId.value = "treeValue";
+        treeValue.setAttributeNode(newClass);
+        treeValue.setAttributeNode(newId)
+        treeValue.textContent = window.treeFigure;
+        
+        document.getElementById("tree").append(treeValue);
+        /*
+        console.log();
+        console.log("Tree Figure");
+        console.log(window.treeFigure);
+        */
+    } else {
+        if (!(document.getElementById("treeValue") === null)){
+            /*
+            console.log();
+            console.log("TreeValue");
+            console.log(document.getElementById("treeValue"));
+            */
+            document.getElementById("treeValue").remove();
+            document.getElementById("tree").remove();
+        }
+        
     }
 
 })
